456712880dbd2de51c3bd4ab8aa374bdcf595121
David Alenoghena <davidalenoghena@gmail.com>
2025-02-25 12:21:11 +0100

    another try

--- sigritto/frontend/src/App.tsx
+++ sigritto/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "@/pages/Home"
-import Navbar from "./components/Navbar"
+import Navbar from "./pages/Navbar"
 import HowItWorks from "@/pages/HowItWorks"
 // Import other pages as needed
 
