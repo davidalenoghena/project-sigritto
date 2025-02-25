014e086accdf0e8c8e3456c8cf28937a6697e101
David Alenoghena <davidalenoghena@gmail.com>
2025-02-25 11:47:53 +0100

    changed to alias format in App.tsx

--- sigritto/frontend/src/App.tsx
+++ sigritto/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import HowItWorks from "./pages/HowItWorks"
+import Navbar from "@/components/Navbar"
+import Home from "@/pages/Home"
+import HowItWorks from "@/pages/HowItWorks"
 // Import other pages as needed
 
 function App() {
